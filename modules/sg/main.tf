af901443709f0ea53a6a14a3ec31a2d92c1561db
Geetha Rashinkar Kumar <geetha.kumar@ravl.io>
2025-09-03 09:33:00 -0400

    changed the vpc id

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   tags = {
     Name = "allow_tls"
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = aws_vpc.my_vpc.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -18,7 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
+  cidr_ipv6         = aws_vpc.my_vpc.ipv6_cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
