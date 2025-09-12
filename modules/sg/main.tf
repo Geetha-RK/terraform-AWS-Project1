8878b37eab97df6dfed506532ef6860a3717d80a
Geetha Rashinkar Kumar <geetha.kumar@ravl.io>
2025-09-12 14:30:58 -0400

    Created security groups and EC2 instance module

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -29,7 +29,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
-  from_port         = 0
-  to_port           = 0
+  ip_protocol       = "-1"  #Allow all ip
+  # from_port         = 0
+  # to_port           = 0
 }
